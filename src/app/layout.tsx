8896b578951b0caa3e760b0838f21a4d948e13be
MU30 <salimusonda1@gmail.com>
2025-07-02 06:12:47 +0000

    default theme should be light, and also when client hovers on the change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,8 +94,7 @@ export default function RootLayout({
       <body className={`${inter.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
           disableTransitionOnChange
         >
           <TranslationProvider>
